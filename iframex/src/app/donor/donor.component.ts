4cfa8a77de554490824942e5bc63015e7b1aa33d
Karthika <j.karthika.bee@gmail.com>
2019-05-20 17:43:27 -0400

    Home screen design & popup messages

--- iframex/src/app/donor/donor.component.ts
+++ iframex/src/app/donor/donor.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AlertsModule, AlertsService } from 'angular-alert-module';
 
 @Component({
   selector: 'app-donor',
@@ -9,16 +10,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 
 export class DonorComponent {
-  donorComp:FormGroup;
-  radioSelectedValue:string;
-  constructor(formBuilder: FormBuilder, httpClient:HttpClient) { 
-   this.donorComp = formBuilder.group({
-     donorCompOpts: ["", Validators.required]
-   });
-   }
-  
+  donorComp: FormGroup;
+  radioSelectedValue: string;
+  router: Router;
+  constructor(formBuilder: FormBuilder, router: Router) {
+    this.donorComp = formBuilder.group({
+      donorCompOpts: ["", Validators.required]
+    });
+    this.router = router;
+  }
+
   //on submit function
-  onClickSubmit(data: { donorCompOpts: string; }){
+  onClickSubmit(data: { donorCompOpts: string; }) {
     this.radioSelectedValue = data.donorCompOpts;
+
+    if (this.radioSelectedValue == 'prepaid_envelope') {
+      //alert(this.radioSelectedValue)
+      this.router.navigate(['../donor']);
+    }
+    else if (this.radioSelectedValue == 'dropoff_location') {
+      //this.alerts.setMessage('dropoff error','error');                                     
+      alert("Please go to drop off location")
+      return false;
+    } else {
+      alert("Please go to home page")
+      return false;
+    }
+    //navigate to the donor-input screen
   }
 }
