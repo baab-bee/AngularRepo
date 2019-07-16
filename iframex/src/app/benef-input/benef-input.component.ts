9b5c90d0de0a9fb8ae3903653c88f690a39682da
sonia <s.targe.bee@gmail.com>
2019-07-16 11:32:36 -0400

    Adding Benefit input screens

--- iframex/src/app/benef-input/benef-input.component.ts
+++ iframex/src/app/benef-input/benef-input.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-benef-input',
@@ -6,10 +9,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./benef-input.component.css']
 })
 export class BenefInputComponent implements OnInit {
+  addForm: FormGroup;
+  userForm: FormGroup;
+  addressForm: FormGroup;
+  rows: FormArray;
+  itemForm: FormGroup;
+  baseUrl = environment.baseUrl;
+  response:any;
+ showMsg: boolean = false;
+ @ViewChild('alert') alert: ElementRef;
+  constructor(private fb: FormBuilder, private http:HttpClient) {
 
-  constructor() { }
+    this.addForm = this.fb.group({
+      userRequestType:['B']
+    });
+  this.userForm = this.fb.group({
+    name:[''],
+    emailId:[''],
+    mobile:['']
+  });
 
-  ngOnInit() {
+  this.addressForm = this.fb.group({
+    addressLine1: [''],
+    city: [''],
+    state:[''],
+    zipcode: [''],
+    country:['']
+  });
+
+    this.rows = this.fb.array([]);
+
   }
 
+  ngOnInit() {
+
+
+        this.addForm.addControl('frameRequests', this.rows);
+   this.addForm.addControl('user',this.userForm);
+   this.userForm.addControl('address', this.addressForm);
+      }
+
+  onAddRow() {
+    this.rows.push(this.createItemFormGroup());
+  }
+
+  onRemoveRow(rowIndex:number){
+    this.rows.removeAt(rowIndex);
+  }
+
+  createItemFormGroup(): FormGroup {
+    return this.fb.group({
+      gender: null,
+      size: null,
+      color: null,
+      material:null
+    });
+  }
+
+  closeAlert() {
+    this.alert.nativeElement.classList.remove('show');
+  }
+    onSubmit(){
+      // const result: UserRequest = Object.assign({}, this.donorForm.value);
+      // result.user= Object.assign({}, result.user);
+      // result.user.address = Object.assign({}, result.user.address);
+  
+      console.log("Form Model is"+JSON.stringify(this.addForm.value));
+     let url = this.baseUrl+ 'userRequests';
+     let observer = this.http.post(url,this.addForm.value,{headers : new HttpHeaders({ 'Content-Type': 'application/json' })});
+     observer.subscribe((response) => { this.response = response;
+      console.log("recieved" +JSON.stringify(this.response));
+      this.showMsg = true;
+              });
+  
+  
+    }
 }
