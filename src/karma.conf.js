8edbb7df54b41a59a606a8ef977fe0f894f5cf50
Karthika <j.karthika.bee@gmail.com>
2019-07-28 20:01:35 -0400

    Removed single run to false

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -26,7 +26,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: false,
     restartOnFileChange: true
   });
 };
