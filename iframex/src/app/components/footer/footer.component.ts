f817dcb52539589d53d82ac9fe2184fdb3753fc4
Karthika <j.karthika.bee@gmail.com>
2019-10-30 23:42:04 -0400

    UI changes

--- iframex/src/app/components/footer/footer.component.ts
+++ iframex/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../../modal.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  modalText: string;
+
+  constructor(private modalService: ModalService) {}
 
   ngOnInit() {
   }
 
+  aboutUs() {
+    this.modalText = "Iframe-X aids automating the collection of used Eyeglass frame donations, distributing them to the people who can't effort to buy. NGO's/Admins operation is hastle free now. Our Software manages to get the matching frames based on beneficiary requirement which helps NGO/Admin to make faster and better decision on distributing the eyeglass frames to the people who can't afford it. <br> Donor can donate used Eyeglass frames smoothly and efficiently. They can opt for pre-enveloped, drop off or seld paid pre-envelope as per thier comfort. <br> Beneficiery who can't afford to have Eyeglass frame  can freely provode thier requirement an recieve well conditioned frame inshort span of time.";
+    this.openModal();
+  }
+
+  contactUs() {
+    this.modalText = "Contact us through Email: baabbee-iframex@gmail.com";
+    this.openModal();
+  }
+
+  help(){
+    window.open("https://www.google.com", "_blank");
+  }
+
+  openModal() {
+    this.modalService.open('custom-modal-1');
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
 }
